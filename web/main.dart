49d979ec39be53fc04b25ca816fb53db0ed704e1
Michał Šrajer <michal.srajer@mobica.com>
2014-12-23 18:09:27 +0100

    movable sprite

--- web/main.dart
+++ web/main.dart
@@ -6,10 +6,12 @@ import 'maze_renderer.dart';
 Maze maze;
 MazeRenderer renderer;
 CanvasElement canvas;
+int playerX;
+int playerY;
 
 void main() {
   maze = new Maze(10, 6);
-  canvas = querySelector("canvas#maze");
+  canvas = querySelector("#maze canvas");
   Element newGame = querySelector("#newGame");
   renderer = new MazeRenderer(canvas);
   renderer.loadSprites("images/maze/sprites64.png", 64).then((_) {
@@ -17,6 +19,7 @@ void main() {
       newGameClick();
     });
     newGameClick();
+    canvas.onKeyDown.listen(keyDown);
   });
 }
 
@@ -24,9 +27,38 @@ void newGameClick() {
   maze.reset();
   var stats = maze.buildDiagonal(0.5);
   print(stats);
+  playerX = maze.start.x;
+  playerY = maze.start.y;
   canvas.height = canvas.height;
   renderer.renderMaze(maze);
   renderer.player_type = MazeRenderer.PLAYER_MOUSE;
-  renderer.renderPlayer(0, 0);
+  renderer.renderPlayer(playerX, playerY);
   renderer.renderGoal(maze.cols - 1, maze.rows - 1);
+  canvas.focus();
+}
+
+
+void keyDown(KeyboardEvent event) {
+  switch (event.keyCode) {
+    case 40: // down
+      playerY++;
+      renderer.renderPlayer(playerX, playerY);
+      event.preventDefault();
+      break;
+    case 38: // up
+      playerY--;
+      renderer.renderPlayer(playerX, playerY);
+      event.preventDefault();
+      break;
+    case 37: // left
+      playerX--;
+      renderer.renderPlayer(playerX, playerY);
+      event.preventDefault();
+      break;
+    case 39: // right
+      playerX++;
+      renderer.renderPlayer(playerX, playerY);
+      event.preventDefault();
+      break;
+  }
 }
--- web/maze_renderer.dart
+++ web/maze_renderer.dart
@@ -6,8 +6,30 @@ import 'dart:async';
 
 import 'maze.dart';
 
+// not used yet
+class Sprite {
+  int sheetX; // on sprite sheet, to me multipied by size
+  int sheetY; // on sprite sheet, to me multipied by size
+  int size; // sprite width and height
+  int x; // on board, in rooms, not pixels
+  int y; // on board, in rooms, not pixels
+  ImageElement image;
+  Sprite(String spriteUrl, this.sheetX, this.sheetY, this.size) {
+    image = new ImageElement(src: spriteUrl);
+    image.style.display = "none";
+    image.style.position = "absolute";
+    image.style.width = "${size}px";
+    image.style.height = "${size}px";
+    image.onLoad.first.then((e) {
+      image.style.left = "${sheetX * size}px";
+      image.style.top = "${sheetY * size}px";
+    });
+  }
+}
+
 class MazeRenderer {
   CanvasElement canvas;
+  Rectangle canvasOffset;
   CanvasRenderingContext2D ctx;
   ImageElement img;
   int size;
@@ -23,18 +45,23 @@ class MazeRenderer {
   int player_type;
   static const int PLAYER_Y = 4;
   static const int GOAL_Y = 5;
-  Point player;
-  Point goal;
+  Element player;
+  Element goal;
 
   MazeRenderer(this.canvas) {
     ctx = canvas.context2D;
     player_type = PLAYER_MOUSE;
+    canvasOffset = canvas.offset;
   }
 
   renderMaze(Maze maze) {
     if (!img.complete) {
       throw "Cannot render maze. Sprites not loaded.";
     }
+    ctx.save();
+    ctx.fillStyle = "#333";
+    ctx.fillRect(0, 0, maze.cols*size, maze.rows*size);
+    ctx.restore();
     Point sprite;
     for (int y = 0; y < maze.rows; y++) {
       for (int x = 0; x < maze.cols; x++) {
@@ -46,11 +73,17 @@ class MazeRenderer {
   }
 
   void renderPlayer(int x, int y) {
-    ctx.drawImageScaledFromSource(img, player_type * size, PLAYER_Y * size, size, size, x * size, y * size, size, size);
+    //ctx.drawImageScaledFromSource(img, player_type * size, PLAYER_Y * size, size, size, x * size, y * size, size, size);
+    player.style.display = "inline-block";
+    player.style.left = "${canvasOffset.left + x * size}px";
+    player.style.top = "${canvasOffset.top + y * size}px";
   }
 
   void renderGoal(int x, int y) {
-    ctx.drawImageScaledFromSource(img, player_type * size, GOAL_Y * size, size, size, x * size, y * size, size, size);
+    //ctx.drawImageScaledFromSource(img, player_type * size, GOAL_Y * size, size, size, x * size, y * size, size, size);
+    goal.style.display = "inline-block";
+    goal.style.left = "${canvasOffset.left + x * size}px";
+    goal.style.top = "${canvasOffset.top + y * size}px";
   }
 
   Future loadSprites(String spriteUrl, int size) {
@@ -59,7 +92,24 @@ class MazeRenderer {
     for (int i = 0; i < sprites.length; i++) {
       sprites[i] = new Point(_spriteX[i] * size, _spriteY[i] * size);
     }
-    var ret = img.onLoad.first;
+    var ret = img.onLoad.first.then((e){
+      player = querySelector("#maze #player");
+      player.style.backgroundImage = "url(${spriteUrl})";
+      player.style.width = "${size}px";
+      player.style.height = "${size}px";
+      player.style.position = "absolute";
+      player.style.backgroundPositionX = "${-player_type * size}px";
+      player.style.backgroundPositionY = "${-PLAYER_Y * size}px";
+      player.style.transition = "150ms";
+      goal = querySelector("#maze #goal");
+      goal.style.backgroundImage = "url(${spriteUrl})";
+      goal.style.width = "${size}px";
+      goal.style.height = "${size}px";
+      goal.style.position = "absolute";
+      goal.style.backgroundPositionX = "${-player_type * size}px";
+      goal.style.backgroundPositionY = "${-GOAL_Y * size}px";
+      goal.style.transition = "150ms";
+    });
     img.src = spriteUrl;
     return ret;
   }
